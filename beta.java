d208bfb4490b7a158e16bf51975a665a2e2e108a
Hassan <47461682+Hassan-A-K@users.noreply.github.com>
2021-03-27 17:59:13 -0600

    junit stuff here

--- beta.java
+++ beta.java
@@ -1,3 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.*;
+import java.util.*;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
 public class beta {
     //Testing out VS code integration
+
+    //test1
+    /*
+    test constructor of inventory
+    uses getters to make sure proper Instantiation of class
+    */
+    @Test
+    public void testConstructor(){
+        Inventory invent = new Inventory("dataBaseURL", "scm", "esnf409");
+        assertTrue("DBURL", "dataBaseURL".equals(invent.getDBURL));
+        assertTrue("USERNAME", "scm".equals(invent.getUSERNAME));
+        assertTrue("PASSWORD", "esnf409".equals(invent.getPASSWORD));
+    }
+
+    //test2
+    /*
+    test initializeConnection
+
+    */
 }
